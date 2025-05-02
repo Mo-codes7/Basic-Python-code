41f1bccae55c9267f50de7c2270ced48214cafb2
Mohamad_Codes <163417825+Mo-codes7@users.noreply.github.com>
2025-05-02 22:38:41 +0100

    Update Speed_Checker.py

--- Speed_Checker.py
+++ Speed_Checker.py
@@ -1,1 +1,12 @@
 
+print("hello world")
+speed = int(input("Enter driver's speed: "))
+
+if speed < 5:
+    print("Speed too low or invalid input.")
+elif speed >= 75:
+    print("Issue speeding fine")
+elif speed > 60:
+    print("Issue warning")
+else:
+    print("No action")
